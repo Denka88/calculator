b2f721e390f87147baa0a963b7160a9598906e64
Denka88 <animedenisik.com@mail.ru>
2024-10-30 22:43:43 +0300

    Создана основа в ветке develop

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        System.out.println("Все операции выполняются соответствующими знаками");
+        System.out.println("После ввода числа или операции нажимать на Enter");
+        Scanner scanner = new Scanner(System.in);
+
+        boolean key = true;
+        while(key){
+
+            int a = scanner.nextInt();
+            char c = scanner.next().charAt(0);
+
+            switch (c) {
+                
+            }
+
+            System.out.print("Закончить?(y/не y - чтобы продолжить): ");
+            char stop = scanner.next().charAt(0);
+            if(stop == 'y'){
+                key = false;
+            }
+
+        }
         
     }
 }

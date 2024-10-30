a63e6290c6bd2354f4c51d30b67cc1042707150c
Denka88 <animedenisik.com@mail.ru>
2024-10-30 22:36:12 +0300

    Реализован метод сложения

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+
+        System.out.println("Все операции выполняются соответствующими знаками");
+        System.out.println("После ввода числа или операции нажимать на Enter");
+        Scanner scanner = new Scanner(System.in);
         
-        
+        boolean key = true;
+        while(key){
+
+            int a = scanner.nextInt();
+            char c = scanner.next().charAt(0);
+
+            switch (c) {
+                case '+':
+                    int b = scanner.nextInt();
+                    System.out.println("Результат: " + sum(a, b));
+                    break;
+            }
+
+            System.out.print("Закончить?(y/не y - чтобы продолжить): ");
+            char stop = scanner.next().charAt(0);
+            if(stop == 'y'){
+                key = false;
+            }
+            
+        }
+
+
+
+
+
+
     }
+
+    public static int sum(int a, int b){
+        return a + b;
+    }
+
 }
